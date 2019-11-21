ab7ce41a6795bc4a9fa091bd0e90659bb10ddc21
matze616 <44054593+matze616@users.noreply.github.com>
2019-11-21 09:34:32 +0100

    3000

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ function getTimestamp(){
     return (dd + '.' + mm + '.' + yyyy + ' ' + hr + ':' + mi + ':' + ss);
 }
 
-http.listen(8080, function () {
+http.listen(3000, function () {
     console.log('listening on *:3000');
 });
 
