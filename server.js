c832ba4f9bf529d268dd81e1e2657762b53f1b4c
matze616 <44054593+matze616@users.noreply.github.com>
2019-12-18 12:31:34 +0100

    Revert "Adding Https support"
    This reverts commit c20da35fa29107a7b3b78a0466b5767fd01a03be.

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 var fs = require('fs');
 var app = express();
-var https = require('https').createServer({
-    key: fs.readFileSync('./server.key'),
-    cert: fs.readFileSync('./server.cert')
-    },app);
-var io = require('socket.io')(https);
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 var uploadid;
 var accesstoken;
 var people = {};
@@ -25,14 +22,14 @@ var visualRecognition = new VisualRecognitionV3({
 const GetAccessToken = new Promise(
     function (resolve, reject) {
         var result;
-        const Https = new XMLHttpRequest();
+        const Http = new XMLHttpRequest();
         const url = 'https://dashdb-txn-sbox-yp-lon02-02.services.eu-gb.bluemix.net/dbapi/v4/auth/tokens';
         var body = JSON.stringify({ "userid": "nqx39539", "password": "j6665@x0vzsk7wgt"});
-        Https.open("POST", url);
-        Https.send(body);
-        Https.onreadystatechange=function(){
+        Http.open("POST", url);
+        Http.send(body);
+        Http.onreadystatechange=function(){
             if(this.readyState==4 && this.status==200){
-                result = JSON.parse(Https.responseText);
+                result = JSON.parse(Http.responseText);
                 resolve(result);
             }
         };
@@ -66,14 +63,14 @@ io.on('connection', function (socket) {
 
             function (resolve, reject) {
                 var result;
-                const Https = new XMLHttpRequest();
+                const Http = new XMLHttpRequest();
                 const url = 'https://eu-de.functions.cloud.ibm.com/api/v1/web/cb82dc99-bde9-4300-900d-ca3e8a0d53f6/hrt-demo/identify-and-translate/?text=' + msg;
-                Https.open("GET", url);
-                Https.send();
+                Http.open("GET", url);
+                Http.send();
 
-                Https.onreadystatechange=function(){
+                Http.onreadystatechange=function(){
                     if(this.readyState==4 && this.status==200){
-                        result = JSON.parse(Https.responseText);
+                        result = JSON.parse(Http.responseText);
                         console.log(result.translations);
                         resolve(result.translations);
                     }
@@ -274,7 +271,7 @@ function getTimestamp(){
     return (dd + '.' + mm + '.' + yyyy + ' ' + hr + ':' + mi + ':' + ss);
 }
 
-https.listen(8080, function () {
+http.listen(8080, function () {
     console.log('listening on *:8080');
 });
 
